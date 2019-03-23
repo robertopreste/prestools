3257b40eb051855b18e5172bf6e318e7fc91976d
Roberto Preste <roberto.preste@gmail.com>
2019-03-23 12:49:03 +0100

    Bump version: 0.1.3 → 0.1.4

--- setup.py
+++ setup.py
@@ -46,6 +46,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/robertopreste/prestools',
-    version='0.1.3',
+    version='0.1.4',
     zip_safe=False,
 )
